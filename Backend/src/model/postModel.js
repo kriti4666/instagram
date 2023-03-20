cf002e53b229ea63c76aa6904c46c77099b4a744
kriti <kritikashyap4666@gmail.com>
2023-03-20 11:48:46 +0530

    follow routes

--- Backend/src/model/postModel.js
+++ Backend/src/model/postModel.js
@@ -1,5 +1,11 @@
+const { appendFile } = require("fs");
 const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema({
-    
+    name: {
+        type: String
+    }
 })
+
+const PostModel = model("post", postSchema)
+module.exports = PostModel
